3b76620bd2547d5d7a5ef43a8e8eede3d4333046
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2022-11-07 22:21:37 +0100

    Redesigned Form 3

--- SzybkaWizyta/SzybkaWizyta/Form3.Designer.cs
+++ SzybkaWizyta/SzybkaWizyta/Form3.Designer.cs
@@ -29,23 +29,33 @@ namespace SzybkaWizyta
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.btnCos = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.btnDoctors = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnHomepage = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel8.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 537);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "KAMIL TU ZAJEB MENU ZAJEBISTE JAK W DRAWMATHIE";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(215)))), ((int)(((byte)(195)))));
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.comboBox1.FormattingEnabled = true;
@@ -54,7 +64,7 @@ namespace SzybkaWizyta
             "frajer",
             "konrad",
             "debil"});
-            this.comboBox1.Location = new System.Drawing.Point(272, 93);
+            this.comboBox1.Location = new System.Drawing.Point(240, 200);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(421, 31);
             this.comboBox1.TabIndex = 1;
@@ -63,47 +73,197 @@ namespace SzybkaWizyta
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(169, 23);
+            this.label2.Font = new System.Drawing.Font("Nexa-Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(20, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
             this.label2.MaximumSize = new System.Drawing.Size(700, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(698, 54);
+            this.label2.Size = new System.Drawing.Size(678, 27);
             this.label2.TabIndex = 5;
             this.label2.Text = "Wybierz specjalizację lekarza, do którego chcesz się zarejestrować:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(187, 495);
+            this.label1.Font = new System.Drawing.Font("Nexa-Bold", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(240, 700);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
             this.label1.MaximumSize = new System.Drawing.Size(700, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(656, 54);
+            this.label1.Size = new System.Drawing.Size(684, 26);
             this.label1.TabIndex = 6;
             this.label1.Text = "Aby wybrać jakiegoś lekarza, należy kliknąć na jego imie lub nazwisko.";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(215)))), ((int)(((byte)(195)))));
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 761);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.btnCos);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(0, 350);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(200, 50);
+            this.panel7.TabIndex = 11;
+            // 
+            // btnCos
+            // 
+            this.btnCos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(246)))), ((int)(((byte)(239)))));
+            this.btnCos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCos.Font = new System.Drawing.Font("Nexa-Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCos.Location = new System.Drawing.Point(0, 0);
+            this.btnCos.Name = "btnCos";
+            this.btnCos.Size = new System.Drawing.Size(200, 50);
+            this.btnCos.TabIndex = 10;
+            this.btnCos.Text = "jatotytytonieja";
+            this.btnCos.UseVisualStyleBackColor = false;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.btnDoctors);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Font = new System.Drawing.Font("Nexa-Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.panel6.Location = new System.Drawing.Point(0, 300);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(200, 50);
+            this.panel6.TabIndex = 10;
+            // 
+            // btnDoctors
+            // 
+            this.btnDoctors.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(246)))), ((int)(((byte)(239)))));
+            this.btnDoctors.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDoctors.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDoctors.Location = new System.Drawing.Point(0, 0);
+            this.btnDoctors.Name = "btnDoctors";
+            this.btnDoctors.Size = new System.Drawing.Size(200, 50);
+            this.btnDoctors.TabIndex = 9;
+            this.btnDoctors.Text = "Lekarze";
+            this.btnDoctors.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btnHomepage);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 250);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(200, 50);
+            this.panel5.TabIndex = 9;
+            // 
+            // btnHomepage
+            // 
+            this.btnHomepage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(246)))), ((int)(((byte)(239)))));
+            this.btnHomepage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHomepage.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHomepage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHomepage.Font = new System.Drawing.Font("Nexa-Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnHomepage.Location = new System.Drawing.Point(0, 0);
+            this.btnHomepage.Name = "btnHomepage";
+            this.btnHomepage.Size = new System.Drawing.Size(200, 50);
+            this.btnHomepage.TabIndex = 9;
+            this.btnHomepage.Text = "Strona Główna";
+            this.btnHomepage.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 200);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 50);
+            this.panel4.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 200);
+            this.panel3.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(215)))), ((int)(((byte)(195)))));
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(808, 80);
+            this.panel2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nexa-Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(40, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(306, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "LEKARZE - WYBIERZ LEKARZA";
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(224)))), ((int)(((byte)(217)))));
+            this.panel8.Controls.Add(this.label2);
+            this.panel8.Location = new System.Drawing.Point(240, 100);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(730, 70);
+            this.panel8.TabIndex = 9;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(246)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(1008, 761);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
             this.Name = "Form3";
             this.Text = "SzybkaWizyta - rejestracja na wizytę";
+            this.panel1.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button btnCos;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button btnDoctors;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnHomepage;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Label label3;
     }
 }
